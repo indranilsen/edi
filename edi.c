e73189d586d13f1ac71396e2ca9325a037e05360
Indranil Sen <indranilsen010@gmail.com>
2019-08-19 22:28:43 -0500

    Added ability to insert characters

--- edi.c
+++ edi.c
@@ -307,6 +307,37 @@ void editorAppendRow(char* s, size_t len) {
     E.num_rows++;
 }
 
+void editorRowInsertChar(erow* row, int at, int c) {
+    // Validate insertion index. It can go one character past the
+    // end of the string, in which case 'c' is appended at the end
+    // of the string.
+    if (at < 0 || at > row->size) {
+        at = row->size;
+    }
+
+    // Allocate an extra byte for the new character + one byte for NULL byte
+    row->chars = realloc(row->chars, row->size + 2);
+
+    // "Split" the contiguous block so there is space for one character between them
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+// ******** EDITOR OPERATIONS ********
+
+void editorInsertChar(int c) {
+    // If the cursor is at the tilde line at the EOF, then append a new row to the file
+    // before inserting a character.
+    if (E.cy == E.num_rows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 // ******** FILE I/O ********
 
 void editorOpen(char* filename) {
@@ -588,6 +619,10 @@ void editorProcessKeypress() {
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+
+        default:
+            editorInsertChar(c);
             break;
     }
 }
